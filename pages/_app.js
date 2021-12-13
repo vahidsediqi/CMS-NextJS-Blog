641a81072c2c57d049a4981e4aee6207aeb54b0c
unknown <you@example.com>
2021-12-13 16:07:16 +0300

    Header component created

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
+import React, { useEffect, useState } from 'React';
+import Layout from '../components/Layout';
+
 import 'tailwindcss/tailwind.css'
+import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
